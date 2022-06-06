622a79faf100ac408206649a8b622c7a4fe8521b
Satyam-05 <96186665+Satyam-05@users.noreply.github.com>
2022-06-07 00:27:09 +0530

    Update WEEK4_Q3.cpp

--- Design Analysis And Algorithm/WEEK4_Q3.cpp
+++ Design Analysis And Algorithm/WEEK4_Q3.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int partition(int arr[], int l, int r)
 {
 	int x = arr[r], i = l;
-	for (int j = l; j <= r - 1; j++)
+	for (int j = l; j <= r -1; j++)
      {
 		if (arr[j] <= x) {
 			swap(arr[i], arr[j]);
